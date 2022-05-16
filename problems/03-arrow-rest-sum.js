161116ae349ce905e1c8dda07299a42e35d207b9
hrj2233 <hrj2233@gmail.com>
2022-05-17 03:22:48 +0900

    complete scope project

---
+++ problems/03-arrow-rest-sum.js
@@ -0,0 +1,25 @@
+/***********************************************************************
+Write a function named `arrowRestSum` that accepts all incoming parameters
+and sums them. Assign the below function to a variable using the const keyword.
+
+**Hint**: Use rest parameter syntax!
+
+Examples:
+arrowRestSum(3,5,6); // => 14
+arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 14
+arrowRestSum(0); // => 0
+***********************************************************************/
+
+// your code here!
+const arrowRestSum = (...arg) => {
+  let sum = 0;
+  arg.forEach((el) => (sum += el));
+  return sum;
+};
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+  module.exports = arrowRestSum;
+} catch (e) {
+  module.exports = undefined;
+}
